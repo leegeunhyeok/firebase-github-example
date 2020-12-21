f7f6394d4af4732997f75da40d4831189c7b7d8b
leegeunhyeok <dev.ghlee@gmail.com>
2020-12-21 17:48:08 +0900

    feat: add code base, sample ui with styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-// TODO
+import firebase from 'firebase/app';
+// Add this line if you enabled Google Analytics.
+// import 'firebase/analytics';
+
+class App {
+  constructor() {
+    this._state = {
+      token: null,
+      username: null,
+    };
+
+    // Your config
+    const firebaseConfig = {
+      apiKey: '',
+      authDomain: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: '',
+      appId: '',
+      measurementId: '',
+    };
+
+    // Firebase initialization
+    firebase.initializeApp(firebaseConfig);
+    // Add this line if you enabled Google Analytics.
+    // firebase.analytics();
+
+    const provider = new firebase.auth.GithubAuthProvider();
+    provider.addScope('repo');
+    this._provider = provider;
+  }
+
+  login() {
+    return firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(({ credential, user }) => {
+        this._state.token = credential.accessToken; // GitHub access token
+        this._state.username = user.username; // GitHub username
+        console.log(user);
+      })
+      .catch((e) => {
+        console.error(e);
+        alert(`${e.code}: ${e.message}`);
+      });
+  }
+
+  getUserInformation() {
+    return new Promise((resolve, reject) => {
+      if (this._state.token) {
+        // TODO: Load user information from GitHub API
+      } else {
+        reject('Not authorized');
+      }
+    });
+  }
+}
+
+const app = new App();
+
+window.onload = () => {
+  document.getElementById('#login').addEventListener('click', () => {
+    app.login();
+  });
+};
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'public'),
     publicPath,
     port: 8080,
   },
